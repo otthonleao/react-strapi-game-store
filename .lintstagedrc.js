3c8d3e058d21d4d3c6fc92954e72af7698ee687b
Otthon Leão <otthonleao@hotmail.com>
2025-06-29 23:07:14 -0400

    feat: add jest to tests

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -2,7 +2,7 @@ module.exports = {
   // Lint & Prettify TS and JS files
   '*.{js,jsx,ts,tsx}': (filenames) => [
     `prettier --write ${filenames.join(' ')}`,
-    // Use Next.js lint command instead of ESLint directly
-    `next lint`
+    `npm run lint --fix . ${filenames.join(' --file')}`,
+    `npm test -- --findRelatedTests ${filenames.join(' ')}`
   ]
 }
---
+++ src/components/Main/index.tsx
@@ -0,0 +1,9 @@
+const Main = () => {
+  return (
+    <div>
+      <h1>React Avançado</h1>
+    </div>
+  )
+}
+
+export default Main
